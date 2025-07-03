e69a0a6bed0d88f6e53a9ea5f4a488dd0f54b10c
juanp <jbaldeoncharri@gmail.com>
2025-07-03 03:23:24 -0500

    Rutas de imagenes actualizado

--- js/productos.js
+++ js/productos.js
@@ -3,7 +3,7 @@ export const productos = [
         id: 1020,
         nombre: "Bikini Animal Print",
         precio: 61.00,
-        imagen: "../assets/img/bikini-animal-print1.jpg",
+        imagen: "../assets/img/bikinis/bikini-animal-print1.jpg",
         descripcion: "Bikini con estampado animal print, ideal para destacar en la playa con estilo y confianza.",
         categoria: "bikini"
     },
@@ -11,7 +11,7 @@ export const productos = [
         id: 1024,
         nombre: "Bikini Blonda",
         precio: 58.00,
-        imagen: "../assets/img/bikini-blonda.jpg",
+        imagen: "../assets/img/bikinis/bikini-blonda.jpg",
         descripcion: "Bikini con detalles de blonda delicada, combinación perfecta de sensualidad y elegancia.",
         categoria: "bikini"
     },
@@ -19,7 +19,7 @@ export const productos = [
         id: 1034,
         nombre: "Bikina Blondita Completa",
         precio: 65.00,
-        imagen: "../assets/img/bikini-blondita-completa.jpg",
+        imagen: "../assets/img/bikinis/bikini-blondita-completa.jpg",
         descripcion: "Conjunto completo de bikini con diseño suave y femenino, para un look delicado.",
         categoria: "bikini"
     },
@@ -27,7 +27,7 @@ export const productos = [
         id: 1022,
         nombre: "Bikini Clasico",
         precio: 56.00,
-        imagen: "../assets/img/bikini-clasico.jpg",
+        imagen: "../assets/img/bikinis/bikini-clasico.jpg",
         descripcion: "Diseño clásico y atemporal, ideal para quienes prefieren un estilo tradicional.",
         categoria: "bikini"
     },
@@ -35,7 +35,7 @@ export const productos = [
         id: 1019,
         nombre: "Bikini Estrella",
         precio: 63.00,
-        imagen: "../assets/img/bikini-estrella.jpg",
+        imagen: "../assets/img/bikinis/bikini-estrella.jpg",
         descripcion: "Bikini con detalles de estrella que aportan un toque juvenil y divertido.",
         categoria: "bikini"
     },
@@ -43,7 +43,7 @@ export const productos = [
         id: 1033,
         nombre: "Bikini Pretina Ancha",
         precio: 62.00,
-        imagen: "../assets/img/bikini-pretina-ancha.jpg",
+        imagen: "../assets/img/bikinis/bikini-pretina-ancha.jpg",
         descripcion: "Cómodo bikini con pretina ancha que brinda mejor ajuste y soporte.",
         categoria: "bikini"
     },
@@ -51,7 +51,7 @@ export const productos = [
         id: 1028,
         nombre: "Cachetero Blonda con Logo",
         precio: 70.00,
-        imagen: "../assets/img/cachetero-blonda-con-logo.jpg",
+        imagen: "../assets/img/cacheteros/cachetero-blonda-con-logo.jpg",
         descripcion: "Cachetero con elegantes acabados en blonda y logo decorativo.",
         categoria: "cachetero"
     },
@@ -59,7 +59,7 @@ export const productos = [
         id: 1029,
         nombre: "Cachetero Blonda",
         precio: 64.00,
-        imagen: "../assets/img/cachetero-blonda-2.jpg",
+        imagen: "../assets/img/cacheteros/cachetero-blonda-2.jpg",
         // imagenes: [
         //     "../assets/img/cachetero-blonda-1.jpg",
         //     "../assets/img/cachetero-blonda-3.jpg",
@@ -72,7 +72,7 @@ export const productos = [
         id: 1072,
         nombre: "Cachetero Corazon Estampado",
         precio: 70.00,
-        imagen: "../assets/img/cachetero-corazon-estampado.jpg",
+        imagen: "../assets/img/cacheteros/cachetero-corazon-estampado-1.jpg",
         descripcion: "Cachetero con estampado de corazones, ideal para un look tierno y coqueto.",
         categoria: "cachetero"
     },
@@ -80,7 +80,7 @@ export const productos = [
         id: 1027,
         nombre: "Cachetero Corazon",
         precio: 65.00,
-        imagen: "../assets/img/cachetero-corazon.jpg",
+        imagen: "../assets/img/cacheteros/cachetero-corazon-1.jpg",
         descripcion: "Cachetero con detalles románticos en forma de corazón, perfecto para ocasiones especiales.",
         categoria: "cachetero"
     },
@@ -88,7 +88,7 @@ export const productos = [
         id: 1052,
         nombre: "Cachetero Dije Estampado",
         precio: 64.00,
-        imagen: "../assets/img/cachetero-dije-estampado.jpg",
+        imagen: "../assets/img/cacheteros/cachetero-dije-estampado-1.jpeg",
         descripcion: "Cachetero con pequeño dije decorativo y estampados modernos.",
         categoria: "cachetero"
     },
@@ -96,7 +96,7 @@ export const productos = [
         id: 1025,
         nombre: "Cachetero Dije",
         precio: 57.00,
-        imagen: "../assets/img/cachetero-dije.jpg",
+        imagen: "../assets/img/cacheteros/cachetero-dije-1.jpeg",
         descripcion: "Cachetero elegante con dije en el frente, para un detalle extra de estilo.",
         categoria: "cachetero"
     },
@@ -104,7 +104,7 @@ export const productos = [
         id: 1050,
         nombre: "Cachetero Encaje Atras",
         precio: 70.00,
-        imagen: "../assets/img/cachetero-encaje-atras.jpg",
+        imagen: "../assets/img/cacheteros/cachetero-encaje-atras-1.jpeg",
         descripcion: "Cachetero con diseño de encaje en la parte posterior, sexy y sofisticado.",
         categoria: "cachetero"
     },
@@ -112,7 +112,7 @@ export const productos = [
         id: 1049,
         nombre: "Cachetero Señorial Floreado",
         precio: 76.00,
-        imagen: "../assets/img/cachetero-señorial-floreado.jpg",
+        imagen: "../assets/img/cacheteros/cachetero-señorial-floreado.jpg",
         descripcion: "Diseño floreado con estilo señorial, mezcla de tradición y elegancia.",
         categoria: "cachetero"
     },
@@ -120,7 +120,7 @@ export const productos = [
         id: 1026,
         nombre: "Semi Hilo Clasico",
         precio: 54.00,
-        imagen: "../assets/img/semi-hilo-clasico.jpg",
+        imagen: "../assets/img/semi/semi-hilo-clasico.jpg",
         descripcion: "Diseño clásico de semi hilo, para quienes buscan comodidad y sensualidad.",
         categoria: "semihilo"
     },
@@ -128,7 +128,7 @@ export const productos = [
         id: 1031,
         nombre: "Semi Hilo Dije",
         precio: 55.00,
-        imagen: "../assets/img/semi-hilo-dije.jpg",
+        imagen: "../assets/img/semi/semi-hilo-dije.jpg",
         descripcion: "Semi hilo decorado con dije, moderno y atractivo para cualquier ocasión.",
         categoria: "semihilo"
     },
@@ -136,7 +136,7 @@ export const productos = [
         id: 1032,
         nombre: "Semi Hilo Pretina Ancha",
         precio: 59.00,
-        imagen: "../assets/img/semi-hilo-pretina-ancha.jpg",
+        imagen: "../assets/img/semi/semi-hilo-pretina-ancha.jpg",
         descripcion: "Semi hilo con pretina ancha que proporciona ajuste ideal y confort.",
         categoria: "semihilo"
     },
@@ -144,7 +144,7 @@ export const productos = [
         id: 1048,
         nombre: "Semi Hilo Señorial Juvenil",
         precio: 73.00,
-        imagen: "../assets/img/semi-señorial-juvenil.jpg",
+        imagen: "../assets/img/semi/semi-señorial-juvenil-1.jpg",
         descripcion: "Estilo que combina elegancia señorial con frescura juvenil.",
         categoria: "semihilo"
     },
@@ -152,7 +152,7 @@ export const productos = [
         id: 1001,
         nombre: "Topsito Clasico",
         precio: 67.00,
-        imagen: "../assets/img/topsito-clasico.jpg",
+        imagen: "../assets/img/topsitos/topsito-clasico.jpg",
         descripcion: "Topsito de corte clásico, esencial para cualquier guardarropa íntimo.",
         categoria: "topsito"
     },
@@ -160,7 +160,7 @@ export const productos = [
         id: 1002,
         nombre: "Topsito con Tirante",
         precio: 67.00,
-        imagen: "../assets/img/topsito-con-tirante.jpg",
+        imagen: "../assets/img/topsitos/topsito-con-tirante.jpg",
         descripcion: "Topsito cómodo con tirantes ajustables, ideal para uso diario.",
         categoria: "topsito"
     },
@@ -168,7 +168,7 @@ export const productos = [
         id: 1003,
         nombre: "Topsito Olimpico",
         precio: 67.00,
-        imagen: "../assets/img/topsito-olimpico.jpg",
+        imagen: "../assets/img/topsitos/topsito-olimpico.jpg",
         descripcion: "Bikini estilo deportivo tipo topsito olímpico, brinda soporte y estilo.",
         categoria: "topsito"
     }
